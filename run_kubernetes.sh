2515fbc58041dbd5ccc949a1b569a5f8683d503f
kani-kalimuthu <kani.cloudstor@gmail.com>
2023-04-01 15:05:03 +0000

    kube_output

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,22 +1,4 @@
 #!/usr/bin/env bash
-
-# This tags and uploads an image to Docker Hub
-
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
-
-# Step 2
-# Run the Docker Hub container with kubernetes
-
-
-# Step 3:
-# List kubernetes pods
-
-# Step 4:
-# Forward the container port to a host
-
-#!/usr/bin/env bash
 dockerpath="kanikalimuthu/udacity"
 
 # Run the Docker Hub container with kubernetes
